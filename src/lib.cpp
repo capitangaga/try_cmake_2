597b23c79e28bf8181959d09bff158894f114570
Kirill Golowko <kirillgolowko@gmail.com>
2018-09-09 18:03:24 +0300

    BAKA

---
+++ src/lib.cpp
@@ -0,0 +1,15 @@
+//
+// Created by kirill on 09.09.18.
+//
+
+#include <string>
+
+
+int find_sum(int a, int b){
+    return a + b;
+}
+
+std::string find_strings_sum(  std::string & a, std::string & b){
+    return a + b;
+}
+
---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by kirill on 09.09.18.
+//
+
+#include <gtest/gtest.h>
+
+#include "../src/lib.cpp"
+
+TEST(testGroup1, test1){
+    ASSERT_EQ(find_sum(2, 3), 5);
+}
+
+
+int main(int argc, char** argv){
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
