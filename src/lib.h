c885bf9a9125b01d3778364da9dc5e74a9af6e27
Kirill Golowko <kirillgolowko@gmail.com>
2018-09-09 18:19:48 +0300

    BAKA

---
+++ src/lib.h
@@ -0,0 +1,14 @@
+//
+// Created by kirill on 09.09.18.
+//
+
+
+#ifndef TEST_TEST_LIB_H
+#define TEST_TEST_LIB_H
+
+int find_sum(int a, int b);
+
+std::string find_strings_sum(  std::string & a, std::string & b);
+
+
+#endif //TEST_TEST_LIB_H
--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,7 @@
 
 #include <gtest/gtest.h>
 
-#include "../src/lib.cpp"
+#include "../src/lib.h"
 
 TEST(testGroup1, test1){
     ASSERT_EQ(find_sum(2, 3), 5);
