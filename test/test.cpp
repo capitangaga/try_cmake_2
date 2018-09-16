627eb9318c443a84a103df634123bab6ab3c0326
Kirill Golowko <kirillgolowko@gmail.com>
2018-09-16 20:44:39 +0300

    BAKA

--- test/test.cpp
+++ test/test.cpp
@@ -3,11 +3,23 @@
 //
 
 #include <gtest/gtest.h>
-
+#include <string>
 #include "../src/lib.h"
 
 TEST(testGroup1, test1){
     ASSERT_EQ(find_sum(2, 3), 5);
+}
+
+
+TEST(testGroup2, test1){
+    std::string a = "a";
+    std::string b = "b";
+    std::string result = "ab";
+    ASSERT_EQ(find_strings_sum(a, b), result);
+}
+
+TEST(testGroup2, test2){
+    ASSERT_EQ(find_sum(3, 3), 6);
 }
 
 
